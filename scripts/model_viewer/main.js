2e225ef18d6ab1b675d731f6f2c22148304d87ab
JuRemmers <jul.remmers@gmail.com>
2018-06-05 15:30:59 +0200

    modellen toegevoegd en geimplementeerd

--- scripts/model_viewer/main.js
+++ scripts/model_viewer/main.js
@@ -1,4 +1,5 @@
 window.onload = init;
+var scene = new THREE.Scene();
 
 //The function that is run once when the page loads
 function init()
@@ -10,7 +11,7 @@ function init()
     customContainer.appendChild(gui.domElement);
 
     //Create scene
-    var scene = new THREE.Scene();
+     scene = new THREE.Scene();
 
     //Create camera
     var camera = createCamera
@@ -26,16 +27,19 @@ function init()
     );
 
     //Load the model
-    loadModel(scene, HARDCODED_3DMODEL_PATH, DEFAULT_MODEL_COLOR, 0.014);
+    var geometry = loadModel(scene, HARDCODED_3DMODEL_PATH, DEFAULT_MODEL_COLOR, 0.014);
 
     //Create a light source (& sphere to visualize source)
-    var light = createPointLight(0.8, 1, 2, 2, gui);
+    var light = createPointLight(0.8, 1, 2, 2);
+
+    //Add a light source to the camera
+    camera.add(light);
+
 
     //Add all models to the scene
     scene.add(camera);
 
-    //Add a light source to the camera
-    camera.add(light);
+
 
     var renderer = createRenderer
     (
@@ -51,9 +55,6 @@ function init()
     orbitControl.enableKeys = false;
     //Disable panning the orbit controller (relative to the model)
     orbitControl.enablePan = false;
-
-    gui.add(light, 'intensity', 0.01, 10);
-    //gui.add(model.rotation, 'z', 0, 10);
 
     //Update the renderer, scene and camera
     update(renderer, scene, camera, orbitControl);
--- views/model_view.php
+++ views/model_view.php
@@ -11,6 +11,34 @@
 </head>
 
 <body>
+    <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <h1>Models</h1>
+    <?php
+    $files = glob("../models/*.obj");
+    foreach($files as $file) {
+      echo "<button class='modelclick' onclick=changeModel('",$file,"')>","<img src='../models/",basename($file, ".obj"), ".png'>","</button>";
+    };
+    ?>
+    </div>
+    <script>
+    /* Set the width of the side navigation to 250px */
+    function changeModel(model){
+      HARDCODED_3DMODEL_PATH = model;
+      console.log(HARDCODED_3DMODEL_PATH);
+      updateModel(scene);
+    }
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "500px";
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+    </script>
+    <span onclick="openNav()">open</span>
     <div id="scene"></div>
     <div id="gui-container"></div>
 </body>
@@ -18,11 +46,10 @@
 <footer>
     <!-- 3RD Party Libraries -->
     <script type="text/javascript" src="../scripts/lib/three.js"></script>
-    <script type="text/javascript" src="../scripts/lib/STLLoader.js"></script>
+    <script type="text/javascript" src="../scripts/lib/OBJLoader.js"></script>
     <script type="text/javascript" src="../scripts/lib/dat.gui.min.js"></script>
     <script type="text/javascript" src="../scripts/lib/orbit.controls.js"></script>
 
-    <script type="text/javascript" src="../scripts/lib/bootstrap/bootstrap.js"></script>
     <script type="text/javascript" src="../scripts/lib/orbit.controls.js"></script>
 
     <!-- Global Variables -->
@@ -33,7 +60,6 @@
     <script type="text/javascript" src="../scripts/controllers/object.controller.js"></script>
     <script type="text/javascript" src="../scripts/controllers/model.controller.js"></script>
     <script type="text/javascript" src="../scripts/controllers/camera.controller.js"></script>
-    <script type="text/javascript" src="../scripts/controllers/gui.controller.js"></script>
 
     <script type="text/javascript" src="../scripts/model_viewer/main.js"></script>
 </footer>
