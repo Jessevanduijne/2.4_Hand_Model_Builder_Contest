9d48cf6896242ee2ef6cbb25df0e8004dea56e39
teijevanhemert <584939@student.inholland.nl>
2018-06-13 15:30:45 +0200

    Exporting File to Server Works - Saved file is zoomed out

--- scripts/controllers/renderer.controller.js
+++ scripts/controllers/renderer.controller.js
@@ -1,5 +1,5 @@
-
 let renderer = null;
+var exporter = null;
 
 function createRenderer(elementId, w, h, color)
 {
@@ -16,8 +16,6 @@ function createRenderer(elementId, w, h, color)
     renderer.setClearColor(color);
     //Enable renderer shadows
     renderer.shadowMap.enabled = true;
-    //Grab the element on which the canvas needs to be rendered
-    console.log(elementId);
     //Grab the provided element and append the renderer
     $('#'+elementId).append(renderer.domElement);
 
@@ -25,9 +23,22 @@ function createRenderer(elementId, w, h, color)
 }
 
 //Save the screenshot
-function save()
+function save_screenshot()
 {
-    //Set the value of the hidden image to a snapshot of the renderer
+    //Set the value of the hidden image input value to a snapshot of the renderer
     $("#hidden_image").val(renderer.domElement.toDataURL('image/png'));
+
+    //Set the value of the hidden model input value to a parse of the scene
+    //Help: This value holds the raw OBJ data
+    $("#hidden_model_input").val(save_model());
+
     $("#image-form").submit();
+}
+
+//Save the screenshot
+function save_model()
+{
+    //Create a new exporter, parse the contents of the scene & return
+    var result = new THREE.OBJExporter().parse(scene);
+    return result;
 }
