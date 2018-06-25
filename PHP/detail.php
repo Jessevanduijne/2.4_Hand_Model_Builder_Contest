805120be7f9b8ab923d52025217e6b46410984b3
Maarten Geerse <mgeerse@gmail.com>
2018-06-25 20:12:59 +0200

    Remove code

--- PHP/detail.php
+++ PHP/detail.php
@@ -101,7 +101,6 @@
     <script>
         function openNav() {
             document.getElementById("mySidenav").style.width = "500px";
-            // $('#nav-icon').hide();
         }
         openNav();
 
